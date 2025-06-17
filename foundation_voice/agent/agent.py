22a743e56bbc5b8317e60554eada0c0accaf9ab8
Aniket <aniket.lnu@think41.com>
2025-06-17 17:13:09 +0530

    Feature/resume session (#38)
    * added the session id in daily
    
    * added the resume_data in contexts
    
    * pass the session id in the events
    
    * revert the custom_plugin changes
    
    * fix custom plugin
    
    * successfully resume the session
    
    * added the sesion and metadata in the connectionhandler too
    
    * fix main
    
    * fix main
    
    * integrate the session_resume and session_id
    
    * fix the requirments.txt
    
    * added the sip_call and remove the transcript from metadat
    
    * sip assistant
    
    * fixed
    
    * remove the $ sign and change the place of metdata_without_trancript
    
    * fixed

--- foundation_voice/agent/agent.py
+++ foundation_voice/agent/agent.py
@@ -134,6 +134,7 @@ async def create_agent_pipeline(
         
         context_aggregator = llm.create_context_aggregator(context)
 
+
         if kwargs.get("sip_params"):
             if kwargs.get("sip_params").get("call_sid"):
                 call_sid = kwargs.get("sip_params").get("call_sid")
@@ -141,7 +142,7 @@ async def create_agent_pipeline(
                 context_aggregator.assistant().add_messages([
                     {
                         "role": "assistant",
-                        "content": f'The call sid is /"${call_sid}/", in case you want use it'
+                        "content": f'The call sid is /"{call_sid}/", in case you want use it'
                     }
                 ])
         else:
@@ -255,15 +256,15 @@ async def create_agent_pipeline(
         observers=task_observers,
     )
 
+    metadata_without_transcript = {}
+    if metadata:
+        metadata_without_transcript = metadata.copy()
+        metadata_without_transcript.pop("transcript", None)
+
     @transcript.event_handler(AgentEvent.TRANSCRIPT_UPDATE.value)
     async def handle_transcript_update(processor, frame):
         callback = callbacks.get_callback(AgentEvent.TRANSCRIPT_UPDATE)
-        
-        metadata_without_transcript = {}
-        if metadata:
-            metadata_without_transcript = metadata.copy()
-            metadata_without_transcript.pop("transcript", None)  
-            
+              
         data = {
             "frame": frame,
             "metadata": metadata_without_transcript,
